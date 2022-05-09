ce970dbb0e33bba079c54d2c4ebe182b9af14ece
lucassouzapb <lycasoliveira@gmail.com>
2022-05-09 11:39:09 -0300

    Criar página inicial de cats #4

---
+++ lib/views/cat/cat_list.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class CatListView extends StatefulWidget {
+  const CatListView({Key? key}) : super(key: key);
+
+  @override
+  State<CatListView> createState() => _CatListViewState();
+}
+
+class _CatListViewState extends State<CatListView> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
