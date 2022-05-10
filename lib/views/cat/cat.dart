4ba521571834862e8b32da381ae64a44089ee27c
lucassouzapb <lycasoliveira@gmail.com>
2022-05-10 15:52:05 -0300

    CRIAR TABBAR DE GATOS #19

--- lib/views/cat/cat.dart
+++ lib/views/cat/cat.dart
@@ -13,104 +13,133 @@ class CatView extends StatefulWidget {
 class _CatViewState extends State<CatView> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.cat.name ?? ""),
-        backgroundColor: Colors.pink.shade100,
-      ),
-      body: Container(
-        padding: EdgeInsets.only(top: 15),
-        child: ListView(
+    return DefaultTabController(
+      length: 2,
+      child: Scaffold(
+        appBar: AppBar(
+          title: Text(widget.cat.name ?? ""),
+          backgroundColor: Colors.pink.shade100,
+          bottom: TabBar(
+            tabs: <Widget>[
+              Tab(
+                text: "Overview",
+              ),
+              Tab(
+                text: "Characteristics",
+              ),
+            ],
+            indicatorColor: Colors.white,
+          ),
+        ),
+        body: TabBarView(
           children: [
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Container(
-                  color: Colors.pink.shade100,
-                  child: Card(
-                    child: Container(
-                      color: Colors.pink.shade50,
-                      child: Column(
-                        children: [
-                          SizedBox(
-                            child: RatingBarIndicator(
-                              rating: double.parse(
-                                  widget.cat!.adaptability.toString()),
-                              itemBuilder: (context, index) => Icon(
-                                Icons.star,
-                                color: Colors.amber,
-                              ),
-                              itemCount: 5,
-                              itemSize: 30,
-                              direction: Axis.horizontal,
-                            ),
-                          ),
-                          SizedBox(
-                            height: 5,
-                          ),
-                          Container(
-                            height: 35,
-                            child: Text(
-                              "Adaptability",
-                              style: TextStyle(
-                                fontSize: 20,
-                                fontWeight: FontWeight.bold,
-                                color: Colors.pink,
-                              ),
-                            ),
-                          ),
-                        ],
-                      ),
-                    ),
-                  ),
-                ),
-                Container(
-                  width: 20,
-                ),
-                Container(
-                  color: Colors.pink.shade100,
-                  child: Card(
-                    child: Container(
-                      color: Colors.pink.shade50,
-                      child: Column(
-                        children: [
-                          SizedBox(
-                            child: RatingBarIndicator(
-                              rating: double.parse(
-                                  widget.cat!.dogFriendly.toString()),
-                              itemBuilder: (context, index) => Icon(
-                                Icons.star,
-                                color: Colors.amber,
-                              ),
-                              itemCount: 5,
-                              itemSize: 30,
-                              direction: Axis.horizontal,
-                            ),
-                          ),
-                          SizedBox(
-                            height: 5,
-                          ),
-                          Container(
-                            height: 35,
-                            child: Text(
-                              "Dog Friendly",
-                              style: TextStyle(
-                                fontSize: 20,
-                                fontWeight: FontWeight.bold,
-                                color: Colors.pink,
-                              ),
-                            ),
-                          ),
-                        ],
-                      ),
-                    ),
-                  ),
-                ),
-              ],
-            )
+            overview(),
+            characteristics(),
           ],
         ),
+
+        /*,*/
       ),
     );
+  }
+
+  Widget characteristics() {
+    return Container(
+      padding: EdgeInsets.only(top: 15),
+      child: ListView(
+        children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Container(
+                color: Colors.pink.shade100,
+                child: Card(
+                  child: Container(
+                    color: Colors.pink.shade50,
+                    child: Column(
+                      children: [
+                        SizedBox(
+                          child: RatingBarIndicator(
+                            rating: double.parse(
+                                widget.cat.adaptability.toString()),
+                            itemBuilder: (context, index) => Icon(
+                              Icons.star,
+                              color: Colors.amber,
+                            ),
+                            itemCount: 5,
+                            itemSize: 30,
+                            direction: Axis.horizontal,
+                          ),
+                        ),
+                        SizedBox(
+                          height: 5,
+                        ),
+                        Container(
+                          height: 35,
+                          child: Text(
+                            "Adaptability",
+                            style: TextStyle(
+                              fontSize: 20,
+                              fontWeight: FontWeight.bold,
+                              color: Colors.pink,
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ),
+              Container(
+                width: 20,
+              ),
+              Container(
+                color: Colors.pink.shade100,
+                child: Card(
+                  child: Container(
+                    color: Colors.pink.shade50,
+                    child: Column(
+                      children: [
+                        SizedBox(
+                          child: RatingBarIndicator(
+                            rating:
+                                double.parse(widget.cat.dogFriendly.toString()),
+                            itemBuilder: (context, index) => Icon(
+                              Icons.star,
+                              color: Colors.amber,
+                            ),
+                            itemCount: 5,
+                            itemSize: 30,
+                            direction: Axis.horizontal,
+                          ),
+                        ),
+                        SizedBox(
+                          height: 5,
+                        ),
+                        Container(
+                          height: 35,
+                          child: Text(
+                            "Dog Friendly",
+                            style: TextStyle(
+                              fontSize: 20,
+                              fontWeight: FontWeight.bold,
+                              color: Colors.pink,
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          )
+        ],
+      ),
+    );
+  }
+
+  Widget overview() {
+    return Container();
   }
 }
