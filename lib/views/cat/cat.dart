d320438cdb172055611b323aeec58b9fc6878274
lucassouzapb <lycasoliveira@gmail.com>
2022-05-10 16:54:34 -0300

    CRIAR TABBAR DE GATOS #19

--- lib/views/cat/cat.dart
+++ lib/views/cat/cat.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_rating_bar/flutter_rating_bar.dart';
 import 'package:wikipets/componentes/rating_card.dart';
 import 'package:wikipets/models/cat_model.dart';
 
--- lib/views/dog/dog.dart
+++ lib/views/dog/dog.dart
@@ -15,7 +15,6 @@ class _DogViewState extends State<DogView> {
     return Scaffold(
       appBar: AppBar(
         title: Text(widget.dog.name ?? ""),
-        backgroundColor: Colors.pink.shade100,
       ),
     );
   }
