b783a5e5d61d1a0beee1b4618421bac8359059c3
lucassouzapb <lycasoliveira@gmail.com>
2022-05-10 11:43:54 -0300

    Arrumar cards de gatos  #12

--- lib/views/cat/cat_list.dart
+++ lib/views/cat/cat_list.dart
@@ -2,8 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:wikipets/models/cat_model.dart';
 import 'package:flutter/src/widgets/image.dart' as img;
-
-import '../../service/cat_service.dart';
+import 'package:wikipets/service/cat_service.dart';
 
 class CatListView extends StatefulWidget {
   const CatListView({Key? key}) : super(key: key);
@@ -26,19 +25,38 @@ class _CatListViewState extends State<CatListView> {
             itemCount: repositorio.cats.length,
             itemBuilder: (BuildContext contexto, int cat) {
               final List<CatModel> lista = repositorio.cats;
-              return ListTile(
-                title: Text(lista[cat].name ?? ""),
-                subtitle: Text(lista[cat].name ?? ""),
-                leading: SizedBox(
-                  width: 50,
-                  height: 50,
-                  child: img.Image.network(lista[cat].image?.url ??
-                      "https://www.petz.com.br/blog/wp-content/uploads/2020/08/cat-sitter-felino.jpg"),
+              return Container(
+                padding: EdgeInsets.only(),
+                child: Card(
+                  clipBehavior: Clip.antiAlias,
+                  child: Column(
+                    children: [
+                      SizedBox(
+                        child: img.Image.network(lista[cat].image?.url ??
+                            "https://www.petz.com.br/blog/wp-content/uploads/2020/08/cat-sitter-felino.jpg"),
+                      ),
+                      Container(
+                        height: 35,
+                        color: Colors.pink.shade100,
+                        child: Center(
+                          child: Text(
+                            lista[cat].name ?? "",
+                            style: TextStyle(
+                              fontSize: 20,
+                              fontWeight: FontWeight.bold,
+                              color: Colors.white,
+                            ),
+                          ),
+                        ),
+                      )
+                    ],
+                  ),
                 ),
-                onTap: () {},
               );
             },
-            separatorBuilder: (_, __) => Divider(),
+            separatorBuilder: (_, __) => Container(
+              height: 5,
+            ),
             padding: EdgeInsets.all(16),
           );
         },
