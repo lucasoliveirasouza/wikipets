d8d59d3a8e7c3d357dde594e5dfe0f108aab8d75
lucasoliveirasouza <>
2022-05-15 14:34:16 -0300

    criar classe usuario #40

--- lib/models/user.dart
+++ lib/models/user.dart
@@ -1,6 +1,18 @@
 class User{
-  String email;
-  String senha;
+  String _email;
+  String _senha;
 
-  User({required this.email, required this.senha});
+  User( this._email, this._senha);
+
+  String get senha => _senha;
+
+  set senha(String value) {
+    _senha = value;
+  }
+
+  String get email => _email;
+
+  set email(String value) {
+    _email = value;
+  }
 }
