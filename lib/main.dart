649ff3894572c56a5b2a7a08c02654fd83dbf311
lucasoliveirasouza <>
2022-05-15 13:15:15 -0300

    Conectar com firebase #35

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,18 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'package:wikipets/firebase_options.dart';
 import 'package:wikipets/service/cat_service.dart';
 import 'package:wikipets/service/dog_service.dart';
 import 'package:wikipets/views/home/home.dart';
 
-void main() {
+void main() async{
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
   runApp(MultiProvider(
     providers: [
       ChangeNotifierProvider(create: (context) => CatService()),
