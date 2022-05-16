fc3a2ad97c134a0408cb06884b93ed5c1c8657c1
lucassouzapb <lycasoliveira@gmail.com>
2022-05-16 14:02:58 -0300

    validar formulário #46

--- lib/views/auth/login.dart
+++ lib/views/auth/login.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 import 'package:provider/provider.dart';
 import 'package:wikipets/constantes.dart';
 import 'package:wikipets/service/auth_service.dart';
@@ -14,93 +15,112 @@ class LoginView extends StatefulWidget {
 class _LoginViewState extends State<LoginView> {
   final senha = TextEditingController();
   final email = TextEditingController();
+  final formKey = GlobalKey<FormState>();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
-        padding: EdgeInsets.only(top: 120, right: 40, left: 40),
-        child: ListView(
-          children: [
-            Center(
-              child: SizedBox(
-                width: 120,
-                height: 120,
-                child: Image.asset("assets/images/cat.png"),
+      body: Form(
+        key: formKey,
+        child: Container(
+          padding: EdgeInsets.only(top: 120, right: 40, left: 40),
+          child: ListView(
+            children: [
+              Center(
+                child: SizedBox(
+                  width: 120,
+                  height: 120,
+                  child: Image.asset("assets/images/cat.png"),
+                ),
               ),
-            ),
-            SizedBox(
-              height: 20,
-            ),
-            Center(
-              child: Text(
-                "Wikipets",
-                style: TextStyle(
-                    fontSize: 25, fontWeight: FontWeight.bold, color: color3),
+              SizedBox(
+                height: 20,
               ),
-            ),
-            SizedBox(
-              height: 20,
-            ),
-            TextFormField(
-              controller: email,
-              decoration: InputDecoration(
-                labelText: "Email",
-                border: OutlineInputBorder(
-                  borderRadius: BorderRadius.all(
-                    new Radius.circular(10.0),
+              Center(
+                child: Text(
+                  "Wikipets",
+                  style: TextStyle(
+                      fontSize: 25, fontWeight: FontWeight.bold, color: color3),
+                ),
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              TextFormField(
+                controller: email,
+                decoration: InputDecoration(
+                  labelText: "Email",
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.all(
+                      new Radius.circular(10.0),
+                    ),
+                  ),
+                  labelStyle: TextStyle(
+                    color: Colors.black38,
+                    fontWeight: FontWeight.w400,
+                    fontSize: 20,
                   ),
                 ),
-                labelStyle: TextStyle(
-                  color: Colors.black38,
-                  fontWeight: FontWeight.w400,
-                  fontSize: 20,
-                ),
-              ),
-            ),
-            SizedBox(
-              height: 25,
-            ),
-            TextFormField(
-              keyboardType: TextInputType.text,
-              obscureText: true,
-              controller: senha,
-              decoration: InputDecoration(
-                labelText: "Password",
-                border: OutlineInputBorder(
-                  borderRadius: BorderRadius.all(
-                    new Radius.circular(10.0),
-                  ),
-                ),
-                labelStyle: TextStyle(
-                  color: Colors.black38,
-                  fontWeight: FontWeight.w400,
-                  fontSize: 20,
-                ),
-              ),
-            ),
-            SizedBox(
-              height: 25,
-            ),
-            Container(
-              height: 55,
-              child: ElevatedButton(
-                onPressed: () {
-                  login();
+                validator: (value) {
+                  if (value!.isEmpty) {
+                    return "Informe o e-mail";
+                  }
+                  return null;
                 },
-                child: Text("Login"),
               ),
-            ),
-            SizedBox(
-              height: 15,
-            ),
-            TextButton(
-              onPressed: () {
-                Navigator.push(context,
-                    MaterialPageRoute(builder: (context) => RegisterView()));
-              },
-              child: Text("Register now"),
-            ),
-          ],
+              SizedBox(
+                height: 25,
+              ),
+              TextFormField(
+                keyboardType: TextInputType.text,
+                obscureText: true,
+                controller: senha,
+                decoration: InputDecoration(
+                  labelText: "Password",
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.all(
+                      new Radius.circular(10.0),
+                    ),
+                  ),
+                  labelStyle: TextStyle(
+                    color: Colors.black38,
+                    fontWeight: FontWeight.w400,
+                    fontSize: 20,
+                  ),
+                ),
+                validator: (value) {
+                  if (value!.isEmpty) {
+                    return "Informe a senha";
+                  }
+                  return null;
+                },
+              ),
+              SizedBox(
+                height: 25,
+              ),
+              Container(
+                height: 55,
+                child: ElevatedButton(
+                  onPressed: () {
+                    if (formKey.currentState!.validate()) {
+                      login();
+                    }
+                  },
+                  child: Text("Login"),
+                ),
+              ),
+              SizedBox(
+                height: 15,
+              ),
+              TextButton(
+                onPressed: () {
+                  Navigator.push(context,
+                      MaterialPageRoute(builder: (context) => RegisterView()));
+                },
+                child: Text("Register now"),
+              ),
+            ],
+          ),
         ),
       ),
     );
@@ -110,8 +130,12 @@ class _LoginViewState extends State<LoginView> {
     try {
       await context.read<AuthService>().login(email.text, senha.text);
     } on AuthException catch (e) {
-      ScaffoldMessenger.of(context)
-          .showSnackBar(SnackBar(content: Text(e.message)));
+      Get.snackbar(
+        "Erro",
+        e.message,
+        backgroundColor: color1,
+        snackPosition: SnackPosition.BOTTOM,
+      );
     }
   }
 }
--- lib/views/auth/register.dart
+++ lib/views/auth/register.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 import 'package:provider/provider.dart';
 import 'package:wikipets/constantes.dart';
 import 'package:wikipets/service/auth_service.dart';
@@ -13,93 +14,112 @@ class RegisterView extends StatefulWidget {
 class _RegisterViewState extends State<RegisterView> {
   final email = TextEditingController();
   final senha = TextEditingController();
+  final formKey = GlobalKey<FormState>();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
-        padding: EdgeInsets.only(top: 120, right: 40, left: 40),
-        child: ListView(
-          children: [
-            Center(
-              child: SizedBox(
-                width: 120,
-                height: 120,
-                child: Image.asset("assets/images/dog.png"),
+      body: Form(
+        key: formKey,
+        child: Container(
+          padding: EdgeInsets.only(top: 120, right: 40, left: 40),
+          child: ListView(
+            children: [
+              Center(
+                child: SizedBox(
+                  width: 120,
+                  height: 120,
+                  child: Image.asset("assets/images/dog.png"),
+                ),
               ),
-            ),
-            SizedBox(
-              height: 20,
-            ),
-            Center(
-              child: Text(
-                "Register",
-                style: TextStyle(
-                    fontSize: 25, fontWeight: FontWeight.bold, color: color3),
+              SizedBox(
+                height: 20,
               ),
-            ),
-            SizedBox(
-              height: 20,
-            ),
-            TextFormField(
-              controller: email,
-              decoration: InputDecoration(
-                labelText: "Email",
-                border: OutlineInputBorder(
-                  borderRadius: BorderRadius.all(
-                    new Radius.circular(10.0),
+              Center(
+                child: Text(
+                  "Register",
+                  style: TextStyle(
+                      fontSize: 25, fontWeight: FontWeight.bold, color: color3),
+                ),
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              TextFormField(
+                controller: email,
+                decoration: InputDecoration(
+                  labelText: "Email",
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.all(
+                      new Radius.circular(10.0),
+                    ),
+                  ),
+                  labelStyle: TextStyle(
+                    color: Colors.black38,
+                    fontWeight: FontWeight.w400,
+                    fontSize: 20,
                   ),
                 ),
-                labelStyle: TextStyle(
-                  color: Colors.black38,
-                  fontWeight: FontWeight.w400,
-                  fontSize: 20,
-                ),
-              ),
-            ),
-            SizedBox(
-              height: 25,
-            ),
-            TextFormField(
-              keyboardType: TextInputType.text,
-              obscureText: true,
-              controller: senha,
-              decoration: InputDecoration(
-                labelText: "Password",
-                border: OutlineInputBorder(
-                  borderRadius: BorderRadius.all(
-                    new Radius.circular(10.0),
-                  ),
-                ),
-                labelStyle: TextStyle(
-                  color: Colors.black38,
-                  fontWeight: FontWeight.w400,
-                  fontSize: 20,
-                ),
-              ),
-            ),
-            SizedBox(
-              height: 25,
-            ),
-            Container(
-              height: 55,
-              child: ElevatedButton(
-                onPressed: () {
-                  registrar();
-                  Navigator.of(context).pop();
+                validator: (value) {
+                  if (value!.isEmpty) {
+                    return "Informe o e-mail";
+                  }
+                  return null;
                 },
-                child: Text("Register"),
               ),
-            ),
-            SizedBox(
-              height: 15,
-            ),
-            TextButton(
-              onPressed: () {
-                Navigator.of(context).pop();
-              },
-              child: Text("Login"),
-            ),
-          ],
+              SizedBox(
+                height: 25,
+              ),
+              TextFormField(
+                keyboardType: TextInputType.text,
+                obscureText: true,
+                controller: senha,
+                decoration: InputDecoration(
+                  labelText: "Password",
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.all(
+                      new Radius.circular(10.0),
+                    ),
+                  ),
+                  labelStyle: TextStyle(
+                    color: Colors.black38,
+                    fontWeight: FontWeight.w400,
+                    fontSize: 20,
+                  ),
+                ),
+                validator: (value) {
+                  if (value!.isEmpty) {
+                    return "Informe a senha";
+                  }
+                  return null;
+                },
+              ),
+              SizedBox(
+                height: 25,
+              ),
+              Container(
+                height: 55,
+                child: ElevatedButton(
+                  onPressed: () {
+                    if (formKey.currentState!.validate()) {
+                      registrar();
+                      Get.back();
+                    }
+                  },
+                  child: Text("Register"),
+                ),
+              ),
+              SizedBox(
+                height: 15,
+              ),
+              TextButton(
+                onPressed: () {
+                  Get.back();
+                },
+                child: Text("Login"),
+              ),
+            ],
+          ),
         ),
       ),
     );
