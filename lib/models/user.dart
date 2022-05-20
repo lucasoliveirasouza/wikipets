9c0c9ac496d308e1d54959726afc4bd123579755
lucassouzapb <lycasoliveira@gmail.com>
2022-05-20 15:14:46 -0300

    Adicionar nome em usuário #66

--- lib/models/user.dart
+++ lib/models/user.dart
@@ -1,13 +1,20 @@
-class User{
+class User {
   String _email;
   String _senha;
+  String _nome;
 
-  User( this._email, this._senha);
+  User(this._email, this._senha, this._nome);
 
   String get senha => _senha;
 
   set senha(String value) {
     _senha = value;
+  }
+
+  String get nome => _nome;
+
+  set nome(String value) {
+    _nome = value;
   }
 
   String get email => _email;
