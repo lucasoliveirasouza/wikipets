832db4f6a75db1c51de6238be2a9b48f2fec995c
lucasoliveirasouza <>
2022-05-29 16:58:58 -0300

    Criar edição de forum no service #88

--- lib/views/forum/forum_edit.dart
+++ lib/views/forum/forum_edit.dart
@@ -17,6 +17,7 @@ class ForumEditView extends StatefulWidget {
 class _ForumEditViewState extends State<ForumEditView> {
   final description = TextEditingController();
   final formKey = GlobalKey<FormState>();
+
   @override
   Widget build(BuildContext context) {
     description.text = widget.forum.description;
@@ -28,57 +29,70 @@ class _ForumEditViewState extends State<ForumEditView> {
       body: Form(
         key: formKey,
         child: Container(
-          padding: EdgeInsets.only(right: 20,left: 20,top: 20),
-          child: ListView(children: [
-            TextFormField(
-              controller: description,
-              maxLines: 4,
-              decoration: InputDecoration(
-                label: Text("Description"),
-                border: OutlineInputBorder(
-                  borderRadius: BorderRadius.all(
-                    new Radius.circular(10.0),
+          padding: EdgeInsets.only(right: 20, left: 20, top: 20),
+          child: ListView(
+            children: [
+              TextFormField(
+                controller: description,
+                maxLines: 4,
+                decoration: InputDecoration(
+                  label: Text("Description"),
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.all(
+                      new Radius.circular(10.0),
+                    ),
+                  ),
+                  labelStyle: TextStyle(
+                    color: Colors.black38,
+                    fontWeight: FontWeight.w400,
+                    fontSize: 20,
                   ),
                 ),
-                labelStyle: TextStyle(
-                  color: Colors.black38,
-                  fontWeight: FontWeight.w400,
-                  fontSize: 20,
-                ),
-              ),
-              validator: (value) {
-                if (value!.isEmpty) {
-                  return "Inform the description";
-                }
-                return null;
-              },
-            ),
-            SizedBox(
-              height: 15,
-            ),
-            Container(
-              height: 50,
-              child: ElevatedButton(
-                onPressed: () {
-                  if (formKey.currentState!.validate()) {
-
-                    Get.snackbar(
-                      "Forum registration",
-                      Provider.of<ForumService>(context, listen: false)
-                          .editForum(widget.forum, description.text)
-                          .toString(),
-                    );
-                    Navigator.of(context).pop();
+                validator: (value) {
+                  if (value!.isEmpty) {
+                    return "Inform the description";
                   }
-
+                  return null;
                 },
-                child: Text(
-                  "Save",
-                  style: TextStyle(fontSize: 20),
+              ),
+              SizedBox(
+                height: 15,
+              ),
+              Container(
+                height: 50,
+                child: ElevatedButton(
+                  onPressed: () {
+                    if (formKey.currentState!.validate()) {
+                      Get.snackbar(
+                        "Forum registration",
+                        Provider.of<ForumService>(context, listen: false)
+                            .editForum(widget.forum, description.text)
+                            .toString(),
+                      );
+                      Navigator.of(context).pop();
+                    }
+                  },
+                  child: Text(
+                    "Save",
+                    style: TextStyle(fontSize: 20),
+                  ),
                 ),
               ),
-            ),
-          ],),
+              SizedBox(
+                height: 25,
+              ),
+              Center(
+                child: IconButton(
+                  onPressed: () {},
+                  icon: Icon(
+                    Icons.delete,
+                    size: 45,
+                    color: color2,
+                  ),
+                ),
+              )
+            ],
+          ),
         ),
       ),
     );
